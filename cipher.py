91706410399fe5a15a3ab9da83ef6ecc284d21ce
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2025-01-03 19:45:53 -0700

    encryoted cipher complete. will move on to cleaning up code and other functions like decryption

--- cipher.py
+++ cipher.py
@@ -1,10 +1,12 @@
 message = "Hello, World!"
 shift = 6
+MAX_CHAR_COUNT = 90
+CHAR_RANGE = 26
 
 #result placeholder
 encrypted_message = " "
 #Go through each letter on the message using a for loop
-for char in message:
+for char in message.upper():
   #if a charcter is NOt a letter in the alphabet then just ignore it
   if char.isalpha():
     #convert the character into its ASCII code
@@ -13,11 +15,19 @@ for char in message:
     #new leter after shifting
     encrypted_code = char_code + shift
 
+    if char_code > MAX_CHAR_COUNT:
+      char_code -= CHAR_RANGE  
+
     #conver the new number into a strg
     encrypted_char = chr(encrypted_code)
 
     #add each new charcater to one another
-    encrypted_message = encrypted_message + encrypted_char
+    encrypted_message += encrypted_char
+
+  else:
+    #adding the non-alpheabetical character as they are
+      encrypted_message += char
+
   
 print(encrypted_message)
 
