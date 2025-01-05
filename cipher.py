45371b1fc9cd1fbd3e5edbf43f7b0dfd09e0a995
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2025-01-04 19:19:42 -0700

    now ciper can decrypt

--- cipher.py
+++ cipher.py
@@ -1,34 +1,38 @@
-message = "Hello, World!"
-shift = 6
-MAX_CHAR_COUNT = 90
+
+MAX_CHAR_CODE = 90
+MIN_CHAR_CODE = 65
 CHAR_RANGE = 26
 
-#result placeholder
-encrypted_message = " "
+
+#lets make this into a function
+def caesar_shift(message, shift):
+
+  #result placeholder
+  encrypted_message = " "
+
 #Go through each letter on the message using a for loop
-for char in message.upper():
-  #if a charcter is NOt a letter in the alphabet then just ignore it
-  if char.isalpha():
-    #convert the character into its ASCII code
-    char_code = ord(char)
+  for char in message.upper():
+    if char.isalpha():   #if a charcter is NO a letter in the alphabet then just ignore it
+      char_code = ord(char)     #convert the character into its ASCII code
 
-    #new leter after shifting
-    encrypted_code = char_code + shift
+      encrypted_code = char_code + shift     #new leter after shifting
 
-    if char_code > MAX_CHAR_COUNT:
-      char_code -= CHAR_RANGE  
+      if char_code > MAX_CHAR_CODE:
+        char_code -= CHAR_RANGE
+        
+      if char_code < MIN_CHAR_CODE:
+        char_code += CHAR_RANGE
 
-    #conver the new number into a strg
-    encrypted_char = chr(encrypted_code)
+      encrypted_char = chr(encrypted_code)     #conver the new number into a strg
+      encrypted_message += encrypted_char    #add each new charcater to one another
 
-    #add each new charcater to one another
-    encrypted_message += encrypted_char
-
-  else:
-    #adding the non-alpheabetical character as they are
-      encrypted_message += char
-
-  
-print(encrypted_message)
+    else:
+      encrypted_message += char     #adding the non-alpheabetical character as they are
+  print(encrypted_message)
 
 
+
+user_message = input("What message would you like to encrypt: ") 
+user_shift = int(input("By what number would you like to shift your message by?: "))
+
+caesar_shift(user_message, user_shift)
